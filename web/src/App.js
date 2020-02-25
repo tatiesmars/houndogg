bac976575bdc6e093178dc6ef0f4689e421bcc6a
tatiesmars <matthieu.couronne@gmail.com>
2020-02-25 10:17:22 +0100

    v1

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,78 @@
-import React from "react";
-import {Image, View, Text } from "react-native";
+import {useEffect, useState} from 'react';
+import {Image, ImageBackground, Text, View} from "react-native";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import {TouchableWithoutFeedback} from "react-native-web";
 
-class App extends React.Component {
-  render() {
+export default function App() {
+    // Declare a new state variable, which we'll call "count"
+    const [images, setImages] = useState([]);
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const db = firebase.firestore();
+        db.collection("contenus").orderBy('position').get().then((querySnapshot) => {
+            setImages(querySnapshot.docs.map(value => value.data().image));
+        })
+    }, []);
+
+    if (!images.length) {
+        return <View/>
+    }
+
+    const onNext = () => {
+        setIndex(index => index === images.length - 1 ? 0 : index + 1)
+
+    }
+
+    const onPrev = () => {
+        setIndex(index => index === 0 ? images.length - 1 : index - 1)
+    }
+
     return (
         <View
             style={{
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "#000000"
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#000000"
             }}
         >
-          <View style={{alignItems: 'center', flex: 1, justifyContent: 'center'  }}>
-              <Image source={require("./images/logoWithText512.png")}  style={{height: 192, width: 291, resizeMode: 'contain'}}/>
-              <Text style={{paddingTop: 12, fontSize: 18 , fontFamily: "Helvetica Neue", color: "#ffffff", textAlign: "center"}}>Coming soon...</Text>
-          </View>
+            <ImageBackground resizeMode="cover" source={{uri: images[index]}}
+                             style={{width: '100%', height: '100%'}}>
+                <View style={{alignItems: 'center', flex: 1, justifyContent: 'center'}}>
+                    <Image source={require("./images/logoWithText512.png")}
+                           style={{height: 192, width: 291, resizeMode: 'contain'}}/>
+                    <Text style={{
+                        paddingTop: 12,
+                        fontSize: 18,
+                        fontFamily: "Helvetica Neue",
+                        color: "#ffffff",
+                        textAlign: "center"
+                    }}>Coming soon...</Text>
+                    <TouchableWithoutFeedback onPress={onPrev}>
+                        <View style={{
+                            position: 'absolute',
+                            left: 0,
+                            top: 0,
+                            height: '100%',
+                            width: '50%',
+                            backgroundColor: 'transparent'
+                        }}/>
+                    </TouchableWithoutFeedback>
+                    <TouchableWithoutFeedback onPress={onNext}>
+                        <View style={{
+                            position: 'absolute',
+                            right: 0,
+                            top: 0,
+                            height: '100%',
+                            width: '50%',
+                            backgroundColor: 'transparent'
+                        }}/>
+                    </TouchableWithoutFeedback>
+                </View>
+            </ImageBackground>
+
         </View>
     );
-  }
 }
-export default App;
--- web/src/index.web.js
+++ web/src/index.web.js
@@ -1,8 +1,22 @@
+
 import './fonts/MonumentExtended-Regular.otf'
 import './fonts/MonumentExtended-Ultrabold.otf'
 import './index.css';
 import { AppRegistry } from 'react-native';
 import App from './App';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAe13oeFs_7lUGqksMhx_Ue2YBb1DBG-zA",
+    authDomain: "houndogg-24837.firebaseapp.com",
+    databaseURL: "https://houndogg-24837.firebaseio.com",
+    projectId: "houndogg-24837",
+    storageBucket: "houndogg-24837.appspot.com",
+    messagingSenderId: "28896623357",
+    appId: "1:28896623357:web:a7bb714e8ffb0659dc5c94",
+    measurementId: "G-R9LEE7YV4C"
+};
+firebase.initializeApp(firebaseConfig);
 
 AppRegistry.registerComponent("App", () => App);
 AppRegistry.runApplication("App", {
