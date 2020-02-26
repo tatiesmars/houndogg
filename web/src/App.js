af116b9b752be0b595eeb965aa60048f79f895bf
tatiesmars <matthieu.couronne@gmail.com>
2020-02-26 11:46:02 +0100

    v2

--- web/src/App.js
+++ web/src/App.js
@@ -1,78 +1,45 @@
-import {useEffect, useState} from 'react';
-import {Image, ImageBackground, Text, View} from "react-native";
+import React, {useEffect, useState} from 'react';
+import {Image, Text, View} from 'react-native';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import './App.scss'
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import {TouchableWithoutFeedback} from "react-native-web";
+
+const StartupScreen = <View
+    style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#000000"
+    }}
+>
+    <View style={{alignItems: 'center', zIndex: 200, flex: 1, justifyContent: 'center'}}>
+        <Image source={require("./images/logoWithText512.png")}
+               style={{height: 192, width: 291, resizeMode: 'contain'}}/>
+        <Text style={{
+            paddingTop: 12,
+            fontSize: 18,
+            fontFamily: "Helvetica Neue",
+            color: "#ffffff",
+            textAlign: "center"
+        }}>Coming soon...</Text>
+    </View>
+</View>
 
 export default function App() {
-    // Declare a new state variable, which we'll call "count"
     const [images, setImages] = useState([]);
-    const [index, setIndex] = useState(0);
 
     useEffect(() => {
         const db = firebase.firestore();
         db.collection("contenus").orderBy('position').get().then((querySnapshot) => {
-            setImages(querySnapshot.docs.map(value => value.data().image));
+            setImages(querySnapshot.docs.map(value => ({
+                source: value.data().image
+            })));
         })
     }, []);
-
-    if (!images.length) {
-        return <View/>
-    }
-
-    const onNext = () => {
-        setIndex(index => index === images.length - 1 ? 0 : index + 1)
-
-    }
-
-    const onPrev = () => {
-        setIndex(index => index === 0 ? images.length - 1 : index - 1)
-    }
-
     return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "#000000"
-            }}
-        >
-            <ImageBackground resizeMode="cover" source={{uri: images[index]}}
-                             style={{width: '100%', height: '100%'}}>
-                <View style={{alignItems: 'center', flex: 1, justifyContent: 'center'}}>
-                    <Image source={require("./images/logoWithText512.png")}
-                           style={{height: 192, width: 291, resizeMode: 'contain'}}/>
-                    <Text style={{
-                        paddingTop: 12,
-                        fontSize: 18,
-                        fontFamily: "Helvetica Neue",
-                        color: "#ffffff",
-                        textAlign: "center"
-                    }}>Coming soon...</Text>
-                    <TouchableWithoutFeedback onPress={onPrev}>
-                        <View style={{
-                            position: 'absolute',
-                            left: 0,
-                            top: 0,
-                            height: '100%',
-                            width: '50%',
-                            backgroundColor: 'transparent'
-                        }}/>
-                    </TouchableWithoutFeedback>
-                    <TouchableWithoutFeedback onPress={onNext}>
-                        <View style={{
-                            position: 'absolute',
-                            right: 0,
-                            top: 0,
-                            height: '100%',
-                            width: '50%',
-                            backgroundColor: 'transparent'
-                        }}/>
-                    </TouchableWithoutFeedback>
-                </View>
-            </ImageBackground>
-
-        </View>
+        <AwesomeSlider bullets={false} organicArrows={false} fillParent media={images} startupScreen={StartupScreen}
+                       startupDelay={1000}/>
     );
 }
